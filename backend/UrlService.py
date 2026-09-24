de485c0f9c0baafa6badb737d5828af383e9cf28
jaynie <jaynieshah2000@gmail.com>
2026-09-24 22:02:48 +0100

    DB orm

--- backend/UrlService.py
+++ backend/UrlService.py
@@ -1,10 +1,7 @@
 from datetime import datetime
 
 from fastapi import HTTPException, Request
-<<<<<<< HEAD
-=======
 from fastapi import FastAPI, HTTPException, Request
->>>>>>> 18e9669ec960c24d1781ea811a812a876f79abc1
 from fastapi.responses import RedirectResponse
 from db import PostgresCRUD as db
 from CacheConn import RedisCache as cache
@@ -89,8 +86,4 @@ class UrlService:
                 status_code=404,
                 detail="Short code not found"
             )
-<<<<<<< HEAD
         return url_data["id"]
-=======
-        return url_data["id"]
->>>>>>> 18e9669ec960c24d1781ea811a812a876f79abc1
